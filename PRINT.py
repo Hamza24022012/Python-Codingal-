6f3ef0022e5b56d3de2ba357eaf353085f79d1c7
Hamza24022012 <hamzawalliulah@gamail.com>
2025-11-07 11:29:31 +0300

    Ln1

---
+++ PRINT.py
@@ -0,0 +1,8 @@
+#printing a greeting message
+print("Hello")
+#printing my name 
+print("My name is Hamza")
+#printing a fun fact about myself
+print("I like playing football")
+#perforn calculation
+print("The result of 855+44 is",855+44)
