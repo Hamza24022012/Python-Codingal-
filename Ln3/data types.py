acaf33b8c4027fb6801fa80a5a534a88a7303436
Hamza24022012 <hamzawalliulah@gamail.com>
2025-11-28 11:26:45 +0300

    Ln3

---
+++ Ln3/data types.py
@@ -0,0 +1,8 @@
+a=9
+print("the type of a is" , type(a))
+b=9.3
+print("the type of b is" , type(b))
+c="code"
+print("the type of c is" , type(c))
+d=True
+print("the type of d is" , type(d))
---
+++ Ln3/type casting.py
@@ -0,0 +1,15 @@
+name="Hamza"
+print("Name :" , type(name))
+age=13
+print("Age :" , type(age))
+is_student=True
+print("Is he a student" , type(is_student))
+weight=52.1
+print("Weight" , type(weight))
+
+print("after typecasting")
+
+age=str(age)
+print (age, type(age))
+weight=int(weight)
+print (weight, type(weight))
