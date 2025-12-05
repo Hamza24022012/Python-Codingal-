77af8258d805d234b579402f9b70eb796c96c15d
Hamza24022012 <hamzawalliulah@gamail.com>
2025-12-05 11:13:08 +0300

    Ln4

---
+++ Ln5/greater or smaller.py
@@ -0,0 +1,7 @@
+i=int(input("Enter a number: "))
+if(i<5):
+    print(i," is smaller than 15")
+    print(i," is in if block")
+else:
+    print(i," is greater than 15")
+    print(i," is in else block")
