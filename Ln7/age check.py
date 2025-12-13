e04b03a347ba7f47434adcc8707fde84aeadc614
Hamza24022012 <hamzawalliulah@gamail.com>
2025-12-13 11:29:39 +0300

    Ln7

---
+++ Ln7/age check.py
@@ -0,0 +1,10 @@
+age=int(input("enter your age"))
+
+if age>=15:
+ print("you are eligible to join")
+else:
+
+  if age<=20:
+    print("you are eligible to join")
+  else:
+    print("you are ineligible to join")
