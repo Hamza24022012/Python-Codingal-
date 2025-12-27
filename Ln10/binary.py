744ff8b20aac10e93757e8ef403aad69000b8647
Hamza24022012 <hamzawalliulah@gamail.com>
2025-12-27 11:50:29 +0300

    afterclass

---
+++ Ln10/binary.py
@@ -0,0 +1,14 @@
+num=int(input("Enter a decimal number:"))
+binary = " "
+
+while num > 0:
+    remainder=num%2
+    temp = ""
+
+    for i in range(1):
+        temp=str(remainder)
+
+    binary= temp+binary
+    num = num//2
+
+print("binary number:",binary)
