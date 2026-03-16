521cfe75692b0fa557a5a7b593875237674f773c
Hamza24022012 <hamzawalliulah@gamail.com>
2026-03-16 14:57:48 +0300

    Ln25

---
+++ Ln25/class dogs.py
@@ -0,0 +1,11 @@
+class dog:
+    species="canine"
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+species1=dog("S1",4)
+species2=dog("S2",5)
+print("Name of species1 is",species1.name,"& age is ",species1.age)
+print("Name of species2 is",species2.name,"& age is ",species2.age)
+print(species1.species)
+print(species2.species)
