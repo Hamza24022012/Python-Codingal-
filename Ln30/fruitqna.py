abc26d1c98db6f7dcb81897dcf0aad73e52e2196
Hamza24022012 <hamzawalliulah@gamail.com>
2026-04-03 11:33:03 +0300

    Ln30

---
+++ Ln30/fruitqna.py
@@ -0,0 +1,7 @@
+import random
+class FruitQuiz:
+    def __init__(self):
+        self.fruits={'apple':'red','orange':'orange','watermelon':'green','banana':'yellow'}
+    def quiz(self):
+        while (True):
+            fruit,color
