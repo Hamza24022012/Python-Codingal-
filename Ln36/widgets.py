2fd3049033c01202c0c1cf8bf242dfae96707d88
Hamza24022012 <hamzawalliulah@gamail.com>
2026-04-25 11:29:40 +0300

    Ln36

--- Ln36/widgets.py
+++ Ln36/widgets.py
@@ -5,26 +5,30 @@ window=Tk()
 window.title("Getting started with widgets")
 window.geometry("400x300")
 
-lbl=Label(text="Hello:)",fg="white",bg="blue",height=1,width=300)
+lbl=Label(text="Here is product)",fg="white",bg="blue",height=1,width=300)
 
-name_lbl=Label(text="Enter your name",bg="#3895D3")
-name_entry=Entry()
+num1=Label(text="Enter 1st num",bg="#3895D3")
+num1_entry=Entry()
+
+num2=Label(text="Enter 2nd num",bg="#3895D3")
+num2_entry=Entry()
 
 def display():
-    name=name_entry.get()
-    global message
-    message="Wecome:) \nToday's date is:"
-    greet="Hello " + name + "\n"
-
-    text_box.insert(END,greet)
-    text_box.insert(END,message)
-    text_box.insert(END,date.today())
+    num1=int(num1_entry.get())
+    num2=int(num2_entry.get())
+    global product
+    result= + num1 * num2
+    product="product is " + str(result)
+    text_box.insert(END,product)
+    
 text_box=Text(height=3)
 btn=Button(text="Begin",command=display,height=1,bg="#3895D3",fg="white")
 
 lbl.pack()
-name_lbl.pack()
-name_entry.pack()
+num1.pack()
+num1_entry.pack()
+num2.pack()
+num2_entry.pack()
 btn.pack()
 text_box.pack()
 
