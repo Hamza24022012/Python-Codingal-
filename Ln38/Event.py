bfb101e06609c8591cdce7ef3e0ea3905ccb37e6
Hamza24022012 <hamzawalliulah@gamail.com>
2026-05-02 11:02:07 +0300

    Ln38

---
+++ Ln38/Event.py
@@ -0,0 +1,16 @@
+from tkinter import *
+window=Tk()
+window.title("Login")
+window.geometry("100x100")
+
+def handle_keypress(event):
+    print(event.char)
+window.bind("<Key>",handle_keypress)
+def handle_click(event):
+    print("\n Button clicked")
+button=Button(text="click me!")
+button.pack()
+button.bind("<Button-1>",handle_click)
+
+
+window.mainloop()
---
+++ Ln38/virus.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import messagebox
+window=Tk()
+window.geometry("200x200")
+
+def msg():
+    messagebox.showwarning("Alert","Virus detected")
+
+button=Button(window,text="Scan for virus",command=msg)
+button.place(x=40,y=80)
+
+window.mainloop()
